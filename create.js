39f7b1fa06bccb06d9f85baba31ab3ec7d795f84
Ang Li <ali52@ucsc.edu>
2019-05-08 02:48:21 -0700

    made some comment in create.js

--- create.js
+++ create.js
@@ -5,3 +5,6 @@ this.player.animations.add('walkUp', Phaser.Animation.generateFrameNames('player
 this.player.animations.add('walkDown', Phaser.Animation.generateFrameNames('player', 4, 7, " ", 2), 4, true);
 this.player.animations.add('walkLeft', Phaser.Animation.generateFrameNames('player', 8, 11, " ", 2), 4, true);
 this.player.animations.add('walkRight', Phaser.Animation.generateFrameNames('player', 12, 15, " ", 2), 4, true);
+
+//No Conflict!!! please!!!!
+//IT is not good!
