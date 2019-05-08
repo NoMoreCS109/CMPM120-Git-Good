aa10051cb8549a2257ee77b3e0baf6196fbd0b40
Ang Li <ali52@ucsc.edu>
2019-05-08 03:51:21 -0700

    Some minor changes in create.js

--- create.js
+++ create.js
@@ -1,10 +1,10 @@
 //create the player sprite
 this.player = this.add.sprite(120, 120, "player_atlas", "child00")
 //add animation
-this.player.animations.add('walkUp', Phaser.Animation.generateFrameNames('player', 0, 3, " ", 2), 4, true);
-this.player.animations.add('walkDown', Phaser.Animation.generateFrameNames('player', 4, 7, " ", 2), 4, true);
-this.player.animations.add('walkLeft', Phaser.Animation.generateFrameNames('player', 8, 11, " ", 2), 4, true);
-this.player.animations.add('walkRight', Phaser.Animation.generateFrameNames('player', 12, 15, " ", 2), 4, true);
+this.player.animations.add('walkUp', Phaser.Animation.generateFrameNames('child', 0, 3, " ", 2), 4, true);
+this.player.animations.add('walkDown', Phaser.Animation.generateFrameNames('child', 4, 7, " ", 2), 4, true);
+this.player.animations.add('walkLeft', Phaser.Animation.generateFrameNames('child', 8, 11, " ", 2), 4, true);
+this.player.animations.add('walkRight', Phaser.Animation.generateFrameNames('child', 12, 15, " ", 2), 4, true);
 
 //No Conflict!!! please!!!!
 //IT is not good!
